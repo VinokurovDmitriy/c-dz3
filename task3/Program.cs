a3d0c4643df4aae9e6554d6f2de2413b52f511c0
dvvinokurov <dmitvitvin@gmail.com>
2022-04-17 17:07:57 +0300

    add negative numbers in task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,14 +1,19 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
-int num = new Random().Next(1, 20);
-int i = 1;
+int num = new Random().Next(-10, 10);
+int current = 1;
+int end = num;
 Console.Write($"{num}-> ");
-while(i <= num){
-    Console.Write($"{i * i * i}");
-    if(i < num){
+if(num < 0){
+    current = num;
+    end = -num;
+}
+while(current <= end){
+    Console.Write($"{current * current * current}");
+    if(current < end){
         Console.Write(", ");
     } else {
          Console.WriteLine(".");
     }
-    i++;
+    current++;
 }
