3e09894c2d3c45ff017a9982411b12562e01f389
VinokurovDmitriy <dmitvitvin@gmail.com>
2022-04-22 13:34:58 +0300

    rename variable in task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,18 +1,18 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
 int num = new Random().Next(-10, 10);
-int current = 1;
+int number = 1;
 int end = num;
 Console.Write($"{num}-> ");
 if (num < 0)
 {
-    current = num;
+    number = num;
     end = -num;
 }
-while (current <= end)
+while (number <= end)
 {
-    Console.Write(current * current * current);
-    if (current < end)
+    Console.Write(number * number * number);
+    if (number < end)
     {
         Console.Write(", ");
     }
@@ -20,5 +20,5 @@ while (current <= end)
     {
         Console.WriteLine(".");
     }
-    current++;
+    number++;
 }
