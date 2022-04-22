4f436efc101c426fb4bab925bc4939e0c4f1744c
VinokurovDmitriy <dmitvitvin@gmail.com>
2022-04-22 13:14:52 +0300

    add new function in task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,8 +1,11 @@
-﻿int a = 14212;
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+int a = 14212;
 int b = 23432;
 int c = 12821;
 
-int getRank(int position, int num)
+//первый способ решения с помощью функций
+int getNumber(int position, int num)
 {
     int i = 5;
     while (i > position)
@@ -15,8 +18,8 @@ int getRank(int position, int num)
 
 void checkPolindrom(int num)
 {
-    int num1 = getRank(1, num);
-    int num2 = getRank(2, num);
+    int num1 = getNumber(1, num);
+    int num2 = getNumber(2, num);
     int num4 = num / 10 % 10;
     int num5 = num % 10;
     Console.Write($"Число {num} ");
@@ -26,7 +29,23 @@ void checkPolindrom(int num)
     }
     Console.WriteLine("является полиндромом");
 }
-
+Console.WriteLine("Это решение с использованием функций checkPolindrom и getNumber");
 checkPolindrom(a);
 checkPolindrom(b);
 checkPolindrom(c);
+Console.WriteLine();
+
+//второй способ решения без использования вспомогательной функции getNumber
+
+Console.WriteLine("Это решение с использованием функций checkPolindrom2");
+
+void checkPolindrom2(int number)
+{
+    Console.Write($"Число {number} ");
+    if (number / 1000 != number % 10 * 10 + number / 10 % 10) Console.Write("не ");
+    Console.WriteLine("палиндром");
+}
+
+checkPolindrom2(a);
+checkPolindrom2(b);
+checkPolindrom2(c);
